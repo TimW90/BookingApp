9bfb73089c2108482d8714137909c9d7db630e24
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-02-19 15:58:42 +0100

    Bullet tracer (not working on Tim's machine)

---
+++ backend/src/main/java/nl/itvitae/BookingApp/hotels/HotelController.java
@@ -0,0 +1,23 @@
+package nl.itvitae.BookingApp.hotels;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+@RequestMapping("api/v1/hotels")
+@RequiredArgsConstructor
+public class HotelController {
+
+    private final HotelRepository hotelRepository;
+
+    @GetMapping
+    public Iterable<Hotel> findAll() {
+        return hotelRepository.findAll();
+    }
+
+    @PostMapping
+    public Hotel newHotel(Hotel hotel) {
+        return hotelRepository.save(hotel);
+    }
+}
