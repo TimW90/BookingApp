8ed7f08c49cada15c86dd96a490112afcdbc5abd
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-02-21 15:29:31 +0100

    Room backend with a Get, Post, Delete and Patch mapping.

---
+++ backend/src/main/java/nl/itvitae/BookingApp/rooms/RoomController.java
@@ -0,0 +1,38 @@
+package nl.itvitae.BookingApp.rooms;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+@RequestMapping("api/v1/rooms")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomRepository roomRepository;
+
+    @GetMapping
+    public Iterable<Room> findAll() {
+        return roomRepository.findAll();
+    }
+
+    @PostMapping
+    public Room newRoom(@RequestBody Room room) {
+        return roomRepository.save(room);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRoom(@PathVariable("id") Long id) {
+        roomRepository.deleteById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Room updateRoom(@PathVariable("id") Long id, @RequestBody Room room) {
+        Room updatedRoom = roomRepository.findById(id).get();
+        updatedRoom.setType(room.getType());
+        updatedRoom.setPrice(room.getPrice());
+        updatedRoom.setAmenities(room.getAmenities());
+        updatedRoom.setLuxury(room.isLuxury());
+        return roomRepository.save(updatedRoom);
+    }
+}
