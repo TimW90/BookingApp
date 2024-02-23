5046bb60af2285263842145b6ce9fff092c1ddd5
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2024-02-23 12:08:29 +0100

    added backend search function

--- backend/src/main/java/nl/itvitae/BookingApp/hotel/HotelController.java
+++ backend/src/main/java/nl/itvitae/BookingApp/hotel/HotelController.java
@@ -3,6 +3,8 @@ package nl.itvitae.BookingApp.hotel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @CrossOrigin("http://localhost:5173")
 @RequestMapping("api/v1/hotels")
@@ -19,5 +21,13 @@ public class HotelController {
     @PostMapping
     public Hotel newHotel(@RequestBody Hotel hotel) {
         return hotelRepository.save(hotel);
+    }
+
+    @GetMapping
+    public Optional<Hotel> getByRandomId() {
+        long count = hotelRepository.count();
+        int maxQuantity = (int)count;
+        int randomID = (int) (Math.random() * count);
+        return hotelRepository.findByRandomId();
     }
 }
--- backend/src/main/java/nl/itvitae/BookingApp/hotel/HotelRepository.java
+++ backend/src/main/java/nl/itvitae/BookingApp/hotel/HotelRepository.java
@@ -3,7 +3,12 @@ package nl.itvitae.BookingApp.hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
+        Optional<Hotel> findByRandomId();
+
 
 }
