363b44b908259a4bb00cccc24c0ca3d9fd83f607
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-02-23 10:44:00 +0100

    Added RoomDTO and an EnumUtil that converts the enum name (All in UPPERCASE) to a better look (first letter UPPER, the rest lowercase and underscores_ turn into spacebars)

--- backend/src/main/java/nl/itvitae/BookingApp/room/RoomDTO.java
+++ backend/src/main/java/nl/itvitae/BookingApp/room/RoomDTO.java
@@ -1,16 +1,21 @@
 package nl.itvitae.BookingApp.room;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Set;
 
-public record RoomDTO(Long id, Room.Type type, BigDecimal price, Set<Room.Amenity> amenities, boolean luxury) {
+import static nl.itvitae.BookingApp.util.EnumUtil.enumName;
+
+public record RoomDTO(Long id, String type, BigDecimal price, String[] amenities, boolean luxury) {
 
     public RoomDTO(Room room) {
         this(
                 room.getId(),
-                room.getType(),
+                enumName(room.getType().name()),
                 room.getPrice(),
-                room.getAmenities(),
+                room.getAmenities().stream()
+                        .map((amenity) -> enumName(amenity.name()))
+                        .toArray(String[]::new),
                 room.isLuxury());
     }
 }
--- backend/src/main/java/nl/itvitae/BookingApp/util/EnumUtil.java
+++ backend/src/main/java/nl/itvitae/BookingApp/util/EnumUtil.java
@@ -2,7 +2,7 @@ package nl.itvitae.BookingApp.util;
 
 public class EnumUtil {
 
-    public static String simpelEnumName(String enumName) {
+    public static String enumName(String enumName) {
         return enumName.substring(0, 1)
                 .toUpperCase() + enumName
                 .substring(1).toLowerCase()
