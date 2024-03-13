239aa62ef38d35071918e39eb50c54ffa08a2cd3
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-03-13 11:49:55 +0100

    Changed the Carousel and it somehow almost 100% works (just need to change the justify-between, start and end)

--- frontend/src/components/room/Carousel.jsx
+++ frontend/src/components/room/Carousel.jsx
@@ -1,21 +1,6 @@
 import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
 
 const Carousel = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    // Is a hashIndex needed? Isn't the useState(0) of currentIndex not enough?
-    // Should [images.length] not be [currentIndex] or no [] at all?
-    // Extract the current index from window.location.hash
-    const hashIndex = parseInt(window.location.hash.substring(6));
-    if (!isNaN(hashIndex) && hashIndex >= 0 && hashIndex < images.length) {
-      setCurrentIndex(hashIndex);
-    }
-    console.log('hashIndex = ' + hashIndex);
-    console.log('currentIndex = ' + currentIndex);
-  }, [images.length]);
-
   return (
     <>
       <div className="carousel mr-5">
@@ -38,20 +23,18 @@ const Carousel = ({ images }) => {
                 >
                   <img src={image.base64Image} alt="Image of the room" />
                   <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    {currentIndex > 0 && (
+                    {index > 0 && (
                       <a
-                        href={'#image' + (currentIndex - 1)}
+                        href={'#image' + (index - 1)}
                         className="btn btn-circle"
-                        onClick={() => setCurrentIndex(currentIndex - 1)}
                       >
                         ❮
                       </a>
                     )}
-                    {currentIndex < images.length - 1 && (
+                    {index < images.length - 1 && (
                       <a
-                        href={'#image' + (currentIndex + 1)}
+                        href={'#image' + (index + 1)}
                         className="btn btn-circle"
-                        onClick={() => setCurrentIndex(currentIndex + 1)}
                       >
                         ❯
                       </a>
