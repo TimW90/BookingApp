af4a39137e5d557124ad185a0658febb7d25a009
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-03-13 10:10:16 +0100

    Working on the carousel

--- frontend/src/components/room/Carousel.jsx
+++ frontend/src/components/room/Carousel.jsx
@@ -1,16 +1,27 @@
-// conditional render the arrows if index = images.length - 1 then don't render > and if index = 0 then don't render <
-
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
 const Carousel = ({ images }) => {
-  console.log(images);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    // Is a hashIndex needed? Isn't the useState(0) of currentIndex not enough?
+    // Should [images.length] not be [currentIndex] or no [] at all?
+    // Extract the current index from window.location.hash
+    const hashIndex = parseInt(window.location.hash.substring(6));
+    if (!isNaN(hashIndex) && hashIndex >= 0 && hashIndex < images.length) {
+      setCurrentIndex(hashIndex);
+    }
+    console.log('hashIndex = ' + hashIndex);
+    console.log('currentIndex = ' + currentIndex);
+  }, [images.length]);
+
   return (
     <>
       <div className="carousel mr-5">
         {(() => {
           // what if images === 0?
           if (images.length === 1) {
-            console.log(images[0]);
             return (
               <img
                 src={images[0].base64Image}
@@ -27,23 +38,24 @@ const Carousel = ({ images }) => {
                 >
                   <img src={image.base64Image} alt="Image of the room" />
                   <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a
-                      href={'#image' + (index == 0 ? (index = 0) : index - 1)}
-                      className="btn btn-circle"
-                    >
-                      ❮
-                    </a>
-                    <a
-                      href={
-                        '#image' +
-                        (index == images.length - 1
-                          ? images.length - 1
-                          : index + 1)
-                      }
-                      className="btn btn-circle"
-                    >
-                      ❯
-                    </a>
+                    {currentIndex > 0 && (
+                      <a
+                        href={'#image' + (currentIndex - 1)}
+                        className="btn btn-circle"
+                        onClick={() => setCurrentIndex(currentIndex - 1)}
+                      >
+                        ❮
+                      </a>
+                    )}
+                    {currentIndex < images.length - 1 && (
+                      <a
+                        href={'#image' + (currentIndex + 1)}
+                        className="btn btn-circle"
+                        onClick={() => setCurrentIndex(currentIndex + 1)}
+                      >
+                        ❯
+                      </a>
+                    )}
                   </div>
                 </div>
               </>
