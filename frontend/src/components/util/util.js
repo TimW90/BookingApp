a61e4e4fc4d11c6d30b81f7ab4d0199fd3509462
Bart Spreij <bartspreij@gmail.com>
2024-03-20 18:41:54 +0100

    Whoops

--- frontend/src/components/util/util.js
+++ frontend/src/components/util/util.js
@@ -1,7 +1,7 @@
 export const capitalize = (string) => {
-  return string[0].toUpperCase() + string.substring(1).replace('_', ' ');
+  return string[0].toUpperCase() + string.substring(1);
 };
 
 export const enumSimpleName = (enumString) => {
-  return capitalize(enumString.toLowerCase());
+  return capitalize(enumString.toLowerCase().replace('_', ' '));
 };
