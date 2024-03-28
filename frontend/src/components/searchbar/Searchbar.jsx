c884291085f1952f4dbf433927dcde9993de47b2
Tim Westerhuis <tim.westerhuis@hotmail.com>
2024-03-28 11:16:45 +0100

    Adjusted Locations margin in the searchbar

--- frontend/src/components/searchbar/Searchbar.jsx
+++ frontend/src/components/searchbar/Searchbar.jsx
@@ -40,7 +40,7 @@ const SearchBar = ({ isRoomSearchBar = false }) => {
   return (
     <div className="flex w-full">
       <form
-        className="flex flex-col sm:flex-row w-full justify-center bg-base-100 gap-4 mx-96 items-center border border-base-content border-opacity-50 rounded-lg my-4 px-4 py-1 z-10 "
+        className="flex flex-col sm:flex-row w-full justify-center bg-base-100 gap-4 mx-72 items-center border border-base-content border-opacity-50 rounded-lg my-4 px-4 py-1 z-10 "
         onSubmit={handleSubmit(onSubmit)}
       >
         {/* This part only renders on an hotel landing page */}
